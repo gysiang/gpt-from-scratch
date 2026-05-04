e435852f79cfde12be895a7fdef394f5c3133612
gysiang <gysiang@gmail.com>
2026-05-04 15:52:43 +0800

    finish

---
+++ gpt.py
@@ -0,0 +1,135 @@
+import torch
+import torch.nn
+from torch.nn import functional as F
+
+batch_size = 64
+block_size = 256
+max_iters = 5000
+eval_interval = 500
+learning_rate = 3e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
+
+torch.manual_seed(1337)
+
+with open('input.txt', 'r', encoding='utf-8') as f:
+	text = f.read()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+stoi = { ch:i for i,ch in enumerate(chars)}
+itos = { i:ch for i, ch in enumerate(chars)}
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: "".join([itos[i] for i in l])
+
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+def get_batch(split):
+	data = train_data if split == 'train' else val_data
+	ix = torch.randint(len(data) - block_size, (batch_size))
+	x = torch.stack([data[i:i+block_size] for i in ix])
+	y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+	return x,y
+
+@torch.no_grad()
+def estimate_loss():
+	out = {}
+	model.eval()
+	for split in ['train', 'val']:
+		losses = torch.zeros(eval_iters)
+		for k in range(eval_iters):
+			X,Y = get_batch(split)
+			logits, loss = model(X,Y)
+			losses[k] = loss.item()
+		out[split] = losses.mean()
+	model.train()
+	return out
+
+class Head(nn.Module):
+
+	def __init__(self, head_size):
+		super().__init__()
+		self.key = nn.Linera(n_embd, head_size, bias=False)
+		self.query = nn.Linear(n_embd, head_size, bias=False)
+		self.value = nn.Linear(n_embd, head_size, bias=False)
+		self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+		self.dropout = nn.Dropout(dropout)
+
+	def forward(self, x):
+
+		B,T,C = x.shape
+		k = self.key(x)
+		q = self.query(x)
+		wei = q @ k.transpose(-2,-1) * k.shape[-1]**-.05
+		wei = wei.maskfill(self.trill[:T,:T] == 0, float('-inf'))
+		wei = F.softmax(wei, dim=-1)
+		wei = self.dropout(wei)
+
+		v = self.value(x)
+		out = wei @ v
+		return out
+
+class MultiHeadAttention(nn.Module):
+
+	def __init__(self, num_heads, head_size):
+		super().__init__()
+		self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+		self.proj = nn.Linear(head_size * num_heads, n_embd)
+
+	def forward(self, x):
+		out = torch.cat([h(x) for h in self.heads], dim=-1)
+		out = self.dropout(self.proj(out))
+		return out
+
+class FeedForward(nn.Module):
+
+	def __init__(self, n_embd):
+		super().__init__()
+		self.net = nn.Sequential(
+			nn.Linear(n_embd, 4 * n_embd),
+			nn.ReLU(),
+			nn.Linear(4 * n_embd, n_embd),
+			nn.Dropout(dropout),
+		)
+
+	def forward(self, x):
+		return self.net(x)
+
+class Block(nn.Module):
+
+	def __init__(self, n_embd, n_head):
+		super().__init__()
+		head_size = n_embd // n_head
+		self.sa = MultiHeadAttention(n_head, head_size)
+		self.ffwd = FeedForward(n_embd)
+		self.ln1 = nn.LayerNorm(n_embd)
+		self.ln2 = nn LayerNorm(n_embd)
+
+
+class GPTLanguageModule(nn.Module):
+
+	def __init__(self):
+		super().__init__()
+		self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+		self.position_embedding_table = nn.Embedding(block_size, n_embd)
+		self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+		self.ln_f = nn.LayerNorm(n_embd)
+		self.lm_head = nn.Linear(n_embd, vocab_size)
+
+		self.apply(self.__init_weights)
+
+	def _init_weights(self, module):
+		if isinstance(module, nn.Linear):
+			torch.nn.init(normal_(module.weight, mean=0.0, std=0.02))
+			if module.bias is not None:
+				torch.nn.init.zeros_(module.bias)
+		elif isinstance(module, nnEmbedding):
+			torch.nn.init.normal_(module.weight, mean=0.0. std=0.02)
